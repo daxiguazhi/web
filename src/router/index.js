75a6d7772e45156be1aee0788292b945bd91edd7
yourdeepmemory <1344770855@qq.com>
2018-03-21 17:49:53 +0800

    注册路由更改

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/page/home'
 import Login from '@/page/login'
+import Register from '@/page/register'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
